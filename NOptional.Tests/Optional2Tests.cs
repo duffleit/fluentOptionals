6a656cac6d409b7a11d760bba34f2fe2c6cd93da
duffleit <davidleitner@live.at>
2016-09-30 02:03:49 +0200

    extended Optional2Tests

--- NOptional.Tests/Optional2Tests.cs
+++ NOptional.Tests/Optional2Tests.cs
@@ -13,10 +13,10 @@ namespace NOptional.Tests
             var noneHandleCalled = false;
 
             Optional.From(1)
-                    .Join("test")
+                    .Join(2)
                     .Match(
-                        some: (p1, p2)  => someHandleCalled = true,
-                        none: ()        => noneHandleCalled = true
+                        some: (p1, p2) => someHandleCalled = true,
+                        none: () => noneHandleCalled = true
                     );
 
             someHandleCalled.Should().BeTrue();
@@ -47,19 +47,72 @@ namespace NOptional.Tests
             var noneHandleCalled = false;
 
             Optional.From(1)
-                    .Join("test")
+                    .Join(2)
                     .Match(
                         some: (p1, p2) =>
                         {
                             someHandleCalled = true;
-                            p1.GetType().Should().Be(typeof (int));
-                            p2.GetType().Should().Be(typeof (string));
+                            p1.Should().Be(1);
+                            p2.Should().Be(2);
                         },
                         none: () => noneHandleCalled = true
                     );
 
             someHandleCalled.Should().BeTrue();
             noneHandleCalled.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Match_WhenAllOptionalsAreSome_ThenMatchReturnsSomeValue()
+        {
+            var x =
+                Optional.From(1)
+                        .Join(2)
+                        .Match(
+                            some: (p1, p2) => "some",
+                            none: () => "none"
+                        );
+
+            x.Should().Be("some");
+        }
+
+
+        [TestMethod]
+        public void Match_WhenOneOptionalIsNone_ThenMatchReturnsNoneValue()
+        {
+            var x =
+                Optional.From(1)
+                        .Join(Optional.None<int>())
+                        .Match(
+                            some: (p1, p2) => "some",
+                            none: () => "none"
+                        );
+
+            x.Should().Be("none");
+        }
+
+        [TestMethod]
+        public void IfNone_WhenOneOptionalIsNone_ThenIfSomeHandleGetsCalled()
+        {
+            var noneHandleCalled = false;
+
+            Optional.From(1)
+                .Join(Optional.None<int>())
+                .IfNone(() => noneHandleCalled = true);
+
+            noneHandleCalled.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IfSome_WhenAllOptionalsAreSome_ThenIfSomeHandleGetsCalled()
+        {
+            var someHandleCalled = false;
+
+            Optional.From(1)
+                .Join(2)
+                .IfSome((p1, p2) => someHandleCalled = true);
+
+            someHandleCalled.Should().BeTrue();
         }
     }
 }
