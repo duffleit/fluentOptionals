3d24748b5920eed31bdfa32a20f1cc1599067868
David <davidleitner@live.at>
2016-10-02 20:57:28 +0200

    changed alled Predicates to Funcs

--- fluentOptionals/Optional.cs
+++ fluentOptionals/Optional.cs
@@ -19,7 +19,7 @@ namespace fluentOptionals
             return (value == null) ? None<T>() : Some(value);
         }
 
-        public static Optional<T> From<T>(T value, Predicate<T> condition)
+        public static Optional<T> From<T>(T value, Func<T, bool> condition)
         {
             if (value == null) return None<T>();
 
@@ -45,7 +45,7 @@ namespace fluentOptionals
             return Optional.From(value);
         }
 
-        public static Optional<T> ToOptional<T>(this T value, Predicate<T> condition)
+        public static Optional<T> ToOptional<T>(this T value, Func<T, bool> condition)
         {
             return Optional.From(value, condition);
         }
