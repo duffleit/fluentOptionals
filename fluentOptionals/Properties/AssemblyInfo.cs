c305654de53787e1a21ba5b5b1ee8d3b8dfbc09f
David <davidleitner@live.at>
2016-10-10 23:25:45 +0200

    moved versioning to appveyor

--- fluentOptionals/Properties/AssemblyInfo.cs
+++ fluentOptionals/Properties/AssemblyInfo.cs
@@ -21,15 +21,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a1dbd4a0-0b9b-48d8-9c95-4d291afaf391")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.32")]
-[assembly: AssemblyFileVersion("1.0.0.32")]
+//versioning done by appveyor build
